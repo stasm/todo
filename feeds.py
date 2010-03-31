cc772dd426375d5ca8b95b9741619c563c597e3b
Stas Malolepszy <stas@mozilla.com>
2010-03-31 18:30:16 +0200

    Clean todo.feeds.Lens

--- feeds.py
+++ feeds.py
@@ -6,29 +6,39 @@ from life.models import Locale
 from todo.models import Project, Todo
 
 class Lens(object):
-    locale = None
-    project = None
-    owner = None
-    
     def __init__(self, args):
-        self.locale = Locale.objects.get(code=args['locale']) if args.has_key('locale') else None
-        self.project = Project.objects.get(slug=args['project']) if args.has_key('project') else None
-        self.owner = Group.objects.get(name=args['owner']) if args.has_key('owner') else None
+        self.locale = None
+        self.project = None
+        self.owner = None
+        self._props = []
+        
+        if args.has_key('locale'):
+            self.locale = Locale.objects.get(code=args['locale'])
+            self._props.append('locale')
+        if args.has_key('project'):
+            self.project = Project.objects.get(slug=args['project'])
+            self._props.append('project')
+        if args.has_key('owner'):
+            self.owner = Group.objects.get(name=args['owner'])
+            self._props.append('owner')
     
-    def for_string(self, prop):
-        return " for %s" % getattr(self, prop) if getattr(self, prop) is not None else ''
-
-    def url_string(self, prop):
-        return "%s:%s" % (prop, getattr(self, prop).code) if getattr(self, prop) is not None else ''
+    def get_for_string(self):
+        string = ''
+        for prop in self._props:
+            string += " for %s" % getattr(self, prop)
+        return string
+        
+    def get_url_string(self):
+        string = ''
+        for prop in self._props:
+            string += "%s:%s" % (prop, getattr(self, prop).code)
+        return string
 
     def filter_queryset(self, q):
-        if self.locale:
-            q = q.filter(locale=self.locale)
-        if self.project:
-            q = q.filter(project=self.project)
-        if self.owner:
-            q = q.filter(owner=self.owner)
-        return q
+        filter_dict = {}
+        for prop in self._props:
+            filter_dict.update({prop: getattr(self, prop)})
+        return q.filter(**filter_dict)
         
 class NewTasksFeed(Feed):
     feed_type = Atom1Feed
@@ -39,20 +49,17 @@ class NewTasksFeed(Feed):
 
     def title(self, lens):
         title = "New tasks"
-        title += lens.for_string('locale')
-        title += lens.for_string('project')
+        title += lens.get_for_string()
         return title
         
     def subtitle(self, lens):
         subtitle = "New l10n tasks (product and web)" 
-        subtitle += lens.for_string('locale')
-        subtitle += lens.for_string('project')
+        subtitle += lens.get_for_string()
         return subtitle
 
     def link(self, lens):
         url = '/todo/feed/tasks/'
-        url += lens.url_string('locale')
-        url += lens.url_string('project')
+        url += lens.get_url_string()
         return url
 
     def item_link(self, task):
