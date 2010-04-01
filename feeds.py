d2ddb77b06863d2eff1e4b52877748dde1ae681d
Stas Malolepszy <stas@mozilla.com>
2010-04-01 13:26:39 +0200

    Allow to specify multiple filter of the same type in the feed view, e.g. /locale:fr,de

--- feeds.py
+++ feeds.py
@@ -5,50 +5,49 @@ from life.models import Locale
 from todo.models import Actor, Project, Todo
 
 class Lens(object):
-    def __init__(self, args):
+    def __init__(self, args, allowed=('locale', 'project')):
         self.locale = None
         self.project = None
         self.owner = None
         self._props = []
         
-        if args.has_key('owner'):
-            self.owner = Actor.objects.get(slug=args['owner'])
+        if 'owner' in allowed and args.has_key('owner'):
+            self.owner = Actor.objects.filter(slug__in=args['owner'].split(','))
             self._props.append('owner')
-        if args.has_key('locale'):
-            self.locale = Locale.objects.get(code=args['locale'])
+        if 'locale' in allowed and args.has_key('locale'):
+            self.locale = Locale.objects.filter(code__in=args['locale'].split(','))
             self._props.append('locale')
-        if args.has_key('project'):
-            self.project = Project.objects.get(slug=args['project'])
+        if 'project' in allowed and args.has_key('project'):
+            self.project = Project.objects.filter(slug__in=args['project'].split(','))
             self._props.append('project')
     
     def get_for_string(self):
         string = ''
         for prop in self._props:
-            string += " for %s" % getattr(self, prop)
+            string += " for %s" % ', '.join([unicode(v) for v in getattr(self, prop)])
         return string
         
     def get_url_string(self):
         string = ''
         for prop in self._props:
-            string += "%s:%s" % (prop, getattr(self, prop).code)
+            string += "%s:%s" % (prop, ','.join([v.code for v in getattr(self, prop)]))
         return string
 
     def filter_queryset(self, q, rel_string='%s'):
         filter_dict = {}
         for prop in self._props:
             if prop == 'owner':
-                filter_dict.update({prop: getattr(self, prop)})
+                filter_dict.update({'%s__in' % prop: getattr(self, prop)})
             else:
-                filter_dict.update({rel_string % prop: getattr(self, prop)})
-        print filter_dict
+                filter_dict.update({'%s__in' % (rel_string % prop): getattr(self, prop)})
         return q.filter(**filter_dict)
         
 class NewTasksFeed(Feed):
     feed_type = Atom1Feed
     
     def get_object(self, bits):
-        args = dict( [ bit.split(':') for bit in bits] )
-        return Lens(args)
+        args = dict( [bit.split(':') for bit in bits] )
+        return Lens(args, ('locale', 'project'))
 
     def title(self, lens):
         title = "New tasks"
@@ -74,6 +73,11 @@ class NewTasksFeed(Feed):
         return tasks.order_by('-pk')[:30]
 
 class NewNextActionsFeed(NewTasksFeed):
+    
+    def get_object(self, bits):
+        args = dict( [bit.split(':') for bit in bits] )
+        return Lens(args, ('owner', 'locale', 'project'))
+        
     def title(self, lens):
         title = "Next actions"
         title += lens.get_for_string()
